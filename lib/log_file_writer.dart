c8d254d2a6addd6ced0f0bfb2fa35e88e65a69d8
Andrew Sheridan <sheridan.andrew13@gmail.com>
2024-02-22 11:01:24 -0700

    Initial commit of LogFileWriter.

--- lib/log_file_writer.dart
+++ lib/log_file_writer.dart
@@ -1,7 +1,70 @@
-library log_file_writer;
+import 'dart:io';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/foundation.dart';
+import 'package:logging/logging.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+
+class LogFileWriter {
+  final Level printLevel;
+  final String Function(DateTime dateTime) fileNameFactory;
+
+  late final File _logFile;
+  late final IOSink _logWriter;
+  final bool _initialized = false;
+
+  String get filePath => _logFile.path;
+  File get logFile => _logFile;
+  bool get initialized => _initialized;
+
+  LogFileWriter({
+    required this.printLevel,
+    required this.fileNameFactory,
+  });
+
+  Future<void> initialize() async {
+    if (!kIsWeb) {
+      final tempDirectory = await getTemporaryDirectory();
+      final fileName = fileNameFactory(DateTime.now());
+      final path = p.join(tempDirectory.path, fileName);
+
+      _logFile = File(path);
+      _logWriter = _logFile.openWrite();
+    }
+
+    Logger.root.onRecord.listen((record) {
+      if (record.level >= printLevel) {
+        final log =
+            "${record.time} [${record.loggerName}] ${record.level.name} - ${record.message}${(record.error == null ? "" : " - ${record.error}")}${(record.stackTrace == null ? "" : "\n${record.stackTrace}\n\n")}";
+
+        if (!kIsWeb) {
+          _logWriter.writeln(log);
+        }
+
+        debugPrint("${_getColorCodeByLogLevel(record.level)}$log");
+      }
+    });
+  }
+
+  static String _getColorCodeByLogLevel(Level level) {
+    // Reset:   \x1B[0m
+    // Black:   \x1B[30m
+    // White:   \x1B[37m
+    // Red:     \x1B[31m
+    // Green:   \x1B[32m
+    // Yellow:  \x1B[33m
+    // Blue:    \x1B[34m
+    // Cyan:    \x1B[36m
+
+    if (level >= Level.SEVERE) return "\x1B[31m";
+    if (level >= Level.WARNING) return "\x1B[33m";
+    if (level >= Level.INFO) return "\x1B[32m";
+    if (level >= Level.FINE) return "\x1B[36m";
+
+    return "\x1B[0m";
+  }
+
+  Future<File> copyLogFileTo(String path) async {
+    return _logFile.copy(path);
+  }
 }
