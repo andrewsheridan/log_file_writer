1e03d3e455dadec9107379a8290772e944ab4832
Andrew Sheridan <sheridan.andrew13@gmail.com>
2025-02-26 15:46:31 -0700

    Fallback email handling.

--- lib/feedback_email_sender.dart
+++ lib/feedback_email_sender.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:log_file_writer/log_file_writer.dart';
 import 'package:logging/logging.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class FeedbackEmailSender {
   final Logger _logger = Logger("FeedbackEmailSender");
@@ -25,8 +26,17 @@ class FeedbackEmailSender {
 
       await FlutterEmailSender.send(email);
     } catch (ex) {
-      _logger.severe("Failed to send email.", ex, StackTrace.current);
-      rethrow;
+      _logger.severe(
+          "Failed to send email via email client.", ex, StackTrace.current);
+      try {
+        final link = "mailto:$recipient?subject=$subjectLine";
+        launchUrlString(link, mode: LaunchMode.externalApplication);
+      } catch (ex) {
+        _logger.severe(
+          "Failed to send email via link.",
+          ex,
+        );
+      }
     }
 
     return true;
