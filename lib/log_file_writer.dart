3b27069e0571d8aeefb599790554551c0ac5cfc3
Andrew Sheridan <sheridan.andrew13@gmail.com>
2025-07-15 14:42:09 -0600

    Update deps

--- lib/log_file_writer.dart
+++ lib/log_file_writer.dart
@@ -55,7 +55,7 @@ class LogFileWriter {
     }
   }
 
-  void _onNewLog(record) {
+  void _onNewLog(LogRecord record) {
     if (record.level < writeToFileLevel && record.level < printToConsoleLevel) {
       return;
     }
